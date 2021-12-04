de9110b996143fa397f556bbfd35223720e9eefd
develinu <iwlee.dev@gmail.com>
2021-12-05 02:17:38 +0900

    사전 학습 내용
    feat : 무료 강의 수강
    
    - React 강의 OT : 왜 리액트가 필요한가
    - 리액트 React 설치와 개발환경 셋팅 (2021 ver)
    - JSX를 이용해 HTML 페이지 제작해보는건 처음이겠죠
    - 중요한 데이터는 변수말고 리액트 state로 만들랬죠
    - 버튼에 기능개발을 해보자 & 리액트 state변경하는 법
    - 숙제 해설 : 블로그 글 수정버튼 만들기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [title, titleModify] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학'])
+  let [likeCount, likeCountModify] = useState(0)
+
+  function titleModifyHandler() {
+    titleModify(['여자 코트 추천', ...title.slice(1,)])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <button onClick={ titleModifyHandler }>버튼</button>
+      <div className="list">
+        <h3> { title[0] } <span onClick={ () => { likeCountModify(++likeCount) } }>👍</span> {likeCount} </h3>
+        <p> 2월 17일 발행 </p>
+        <hr/>
+        <h3> { title[1] } </h3>
+        <p> 2월 18일 발행 </p>
+        <hr/>
+        <h3> { title[2] } </h3>
+        <p> 2월 19일 발행 </p>
+        <hr/>
+      </div>
     </div>
   );
 }
